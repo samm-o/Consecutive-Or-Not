5780d13bc726385a4f110a5a38db4d4640f799ac
samm-o <67648665+samm-o@users.noreply.github.com>
2023-01-30 11:01:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Consecutive_Or_Not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter integers seperated by a hyphen: ");
+            var input = Console.ReadLine();
+            while (true)
+            {
+                if (input == "")
+                {
+                    Console.WriteLine("Empty List.");
+                    break;
+                }
+                var numbers = Array.ConvertAll(input.Split('-'), int.Parse).ToList();
+                numbers.Sort();
+
+                if (numbers.Count == 1)
+                {
+                    Console.WriteLine("Only one number.");
+                    break;
+                }
+                else if (numbers[0] + numbers.Count - 1 == numbers.Last())
+                {
+                    Console.WriteLine("Consecutive.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not Consecutive");
+                    break;
+                }
+            }
+            
+            
+
+        }
+    }
+}
